a301b51c6026b2637c139a74197c8ff2e2bf06c1
Nikola <nstipic@etfos.hr>
2021-01-14 19:53:48 +0100

    Finished Lv4

---
+++ RUAP_LV4/UnitTest2.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace RUAP_LV4
+{
+    class UnitTest2
+    {
+
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            baseURL = "https://www.google.com/";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void TheCase1Test()
+        {
+            driver.Navigate().GoToUrl("http://demowebshop.tricentis.com/");
+            driver.FindElement(By.XPath("//body")).Click();
+            driver.FindElement(By.LinkText("Books")).Click();
+            driver.FindElement(By.XPath("//body")).Click();
+            driver.FindElement(By.LinkText("Computing and Internet")).Click();
+            driver.FindElement(By.Id("add-to-cart-button-13")).Click();
+            driver.FindElement(By.XPath("//li[@id='topcartlink']/a/span")).Click();
+            driver.FindElement(By.XPath("(//a[contains(text(),'Computing and Internet')])[2]")).Click();
+        }
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
